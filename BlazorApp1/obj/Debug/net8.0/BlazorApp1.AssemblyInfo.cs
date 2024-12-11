35654aa9ba6f134d1947fb5103fb8eb88473894a
yanlikliff2 <167241536+yanlikliff2@users.noreply.github.com>
2024-12-11 13:33:54 +0300

    первое исправление

--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+841ddf40e0a76fb5d26eb1bf02a9fe9bb0e07e1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6164d7cb5fea38badc1ce0e2b0e185842d318084")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
