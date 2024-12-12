4628019b8c61a761ec500b96bcfe2fadd1a1b97a
yanlikliff2 <167241536+yanlikliff2@users.noreply.github.com>
2024-12-12 09:19:18 +0300

    да

--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6664c1f6acd22dfe126f255d6605a959dff8616d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+234887f4716b6ff2e98d90de6ee88aa072ec9bb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
