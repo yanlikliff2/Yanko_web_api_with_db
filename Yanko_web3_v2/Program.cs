a8c6d13960ad161db333e80b077ae5e61821cf7e
yanlikliff2 <167241536+yanlikliff2@users.noreply.github.com>
2024-12-18 22:22:35 +0300

    б

--- Yanko_web3_v2/Program.cs
+++ Yanko_web3_v2/Program.cs
@@ -9,7 +9,6 @@ using System.Reflection;
 using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using Yanko_web3_v2.Models;
-using Yanko_web3_v2.Sevices;
 
 namespace Yanko_web3_v2
 {
@@ -21,93 +20,23 @@ namespace Yanko_web3_v2
 
 
             builder.Services.AddDbContext<PractDbContext>(
-               options => options.UseSqlServer(builder.Configuration["ConnectionString"]));
-
+                options => options.UseSqlServer(builder.Configuration["ConnectionString"]));
             // Add services to the container.
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
-
-            builder.Services.AddScoped<IJwtUtils, JwtUtils>();
-            builder.Services.AddScoped<IAccountService, AccountssService>();
-            builder.Services.AddScoped<IEmailService, EmailService>();
-
-            builder.Services.AddControllers().AddJsonOptions(x =>
-            {
+            builder.Services.AddControllers().AddJsonOptions(x => {
                 x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-            });
+                });
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/as
+            // pnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddMapster();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            using (var scope = app.Services.CreateScope())
+            using (var scope = app.Services.CreateScope()) 
             {
                 var services = scope.ServiceProvider;
-                var context = services.GetRequiredService<PractDbContext>();
-                context.Database.MigrateAsync();
-            }
-
-            builder.Services.AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Version = "v1",
-                    Title = "Backend API",
-                    Description = "Backend API ASP .NET Core Web API",
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Internet Shop",
-                        Url = new Uri("https://example.com/contact")
-                    },
-                    License = new OpenApiLicense
-                    {
-                        Name = "Internet Shop",
-                        Url = new Uri("https://example.com/license")
-                    },
-                });
-                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
-                      Enter 'Bearer' [space] and then your token in the text input below.
-                      \r\n\r\nExample: 'Bearer 12345abcdef'",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer"
-                });
-                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
-                      {
-                    {
-                      new OpenApiSecurityScheme
-                      {
-                        Reference = new OpenApiReference
-                          {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
-                          },
-                          Scheme = "oauth2",
-                          Name = "Bearer",
-                          In = ParameterLocation.Header,
-
-                        },
-                        new List<string>()
-                      }
-                        });
-                // using System.Reflection;
-                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-            });
-
-
-            using (var scope = app.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-
                 var context = services.GetRequiredService<PractDbContext>();
                 context.Database.Migrate();
             }
@@ -118,17 +47,24 @@ namespace Yanko_web3_v2
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-            app.UseCors(builder => builder.WithOrigins(new[] { "https://localhost:7082", })
+
+            app.UseCors(builder => builder.WithOrigins(new[] { "https://localhost:7098", "https://yanko-web-api-with-db.onrender.com" })
             .AllowAnyHeader()
             .AllowAnyOrigin()
-            .AllowAnyMethod());
+            .AllowAnyMethod()
+            );
+
+
+            app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
-            app.UseHttpsRedirection();
             app.UseMiddleware<ErrorHandlerMidleware>();
-            app.UseMiddleware<JwtMiddleware>();
+
+            app.UseMiddleware<IJwtUtils>();
+
             app.MapControllers();
+
             app.Run();
         }
     }
