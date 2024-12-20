95b1f9c32898b490c1247bfef2de322209116bf2
yanlikliff2 <167241536+yanlikliff2@users.noreply.github.com>
2024-12-20 11:26:58 +0300

    b

--- Yanko_web3_v2/obj/Debug/net8.0/Yanko_web3_v2.AssemblyInfo.cs
+++ Yanko_web3_v2/obj/Debug/net8.0/Yanko_web3_v2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Yanko_web3_v2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd85de5663300d5fe5331f7c01d81abf194cdaa6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61567830212d0c5f0df4f8419d21b3cf46c895df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Yanko_web3_v2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Yanko_web3_v2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
